f985c1c3a6dfec2e8b8d34dff1069515d4d9209e
Manu Sharma <sharma.manu.co@gmail.com>
2025-03-10 08:08:47 +0530

    Changes in main file

--- src/main/java/com/project/LibraryManagementSystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/project/LibraryManagementSystem/LibraryManagementSystemApplication.java
@@ -2,9 +2,13 @@ package com.project.LibraryManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.project.LibraryManagementSystem.repository")
+@EntityScan(basePackages = "com.project.LibraryManagementSystem.model")
 @EnableAspectJAutoProxy
 public class LibraryManagementSystemApplication {
 
